feb388c40e2793d582de5fa9d7b82e7e0c92b80c
learntechpuzz <learntechpuzz@gmail.com>
2020-08-29 21:12:16 +0530

    changes

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import AuthService from 'src/services/auth.service';
 
-
 export const PrivateRoute = ({ component: Component, roles, ...rest }) => {
-    const navigate = useNavigate();
     const currentUser = AuthService.getCurrentUser();
     if (!currentUser) {
         // not logged in so redirect to login page with the return url
-        navigate("/login", { replace: true });
+        return (<Navigate to="/login" replace={true} />);
     }
     // check if route is restricted by role
     if (currentUser && currentUser.roles && roles && !currentUser.roles.some(r => roles.includes(r)) === true) {
         // role not authorised so redirect to home page
         AuthService.logout();
-        navigate("/login", { replace: true });
+        return (<Navigate to="/login" replace={true} />);
     }
-    
+
     return (<Component {...rest} />);
 }
